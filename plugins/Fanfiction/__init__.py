f19eb443ac94cd060a76b2fd176948b5a5da75d2
Riley Flynn <riley@rileyflynn.me>
2016-12-15 16:46:08 -0330

    Changed the Fanfiction plugin so it doesn't retrieve the feature every time it is used.

--- plugins/Fanfiction/__init__.py
+++ plugins/Fanfiction/__init__.py
@@ -11,7 +11,7 @@ from NintbotForDiscord.EventArgs import CommandReceivedEventArgs
 class Plugin(BasePlugin):
     PLUGIN_NAME = "Fanfiction"
     PLUGIN_DESCRIPTION = "Generates fanfiction based on users in the server"
-    PLUGIN_VERSION = "1.1"
+    PLUGIN_VERSION = "1.1.1"
     PLUGIN_DEVELOPER = "nint8835"
 
     def __init__(self, bot: "Bot.Bot", folder: os.path):
@@ -23,11 +23,10 @@ class Plugin(BasePlugin):
                                                  self.generate_fanfiction,
                                                  self)
 
-        self.bot.FeatureManager.register_feature(self, "fanfiction")
+        self.feature = self.bot.FeatureManager.register_feature(self, "fanfiction")
 
     async def generate_fanfiction(self, args: CommandReceivedEventArgs):
-        feature = self.bot.FeatureManager.get_feature("fanfiction")
-        if await feature.feature_enabled(args.channel.server):
+        if await self.feature.feature_enabled(args.channel.server):
             fanfiction = random.choice(self.fanfiction)
             users = []
             for i in range(fanfiction["members"]):
